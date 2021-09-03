a7f56802698d8aa14dc657683252c8729e42a239
sisilet <astroboykl@yahoo.com>
2021-09-03 23:53:13 +1000

    modi

--- application-workloads/obs/obs-studio-stream-vm-chocolatey/ChocoInstall.ps1
+++ application-workloads/obs/obs-studio-stream-vm-chocolatey/ChocoInstall.ps1
@@ -7,18 +7,20 @@ Write-Host "File packages URL: $linktopackages"
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
 
 #Change securoty protocol
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+#[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 
 # Install Choco
 $sb = { iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) }
 Invoke-Command -ScriptBlock $sb 
 
-$sb = { Set-ItemProperty -path HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System -name EnableLua -value 0 }
-Invoke-Command -ScriptBlock $sb 
+#$sb = { Set-ItemProperty -path HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System -name EnableLua -value 0 }
+#Invoke-Command -ScriptBlock $sb 
 
 #Install Chocolatey Packages
 $chocoPackages.Split(";") | ForEach {
     choco install $_ -y -force
 }
+
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\ServerManager" -Name "DoNotOpenServerManagerAtLogon" -Value 1
 
 Write-Host "Packages from choco.org were installed"
