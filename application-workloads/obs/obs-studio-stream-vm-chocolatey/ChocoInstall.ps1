b9b1ced66407af193e3ed65d813ca2bd27d470ae
sisilet <astroboykl@yahoo.com>
2021-09-04 22:01:06 +1000

    Update ChocoInstall.ps1

--- application-workloads/obs/obs-studio-stream-vm-chocolatey/ChocoInstall.ps1
+++ application-workloads/obs/obs-studio-stream-vm-chocolatey/ChocoInstall.ps1
@@ -4,22 +4,22 @@ param([Parameter(Mandatory=$true)][string]$chocoPackages)
 Write-Host "File packages URL: $linktopackages"
 
 #Changing ExecutionPolicy
-Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
+Set-ExecutionPolicy Bypass -Scope Process -Force
 
 #Change securoty protocol
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 
 # Install Choco
-$sb = { iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) }
+$sb = { iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')) }
 Invoke-Command -ScriptBlock $sb 
 
-$sb = { Set-ItemProperty -path HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System -name EnableLua -value 0 }
-Invoke-Command -ScriptBlock $sb 
+# $sb = { Set-ItemProperty -path HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System -name EnableLua -value 0 }
+# Invoke-Command -ScriptBlock $sb 
 
 #Install Chocolatey Packages
-$chocoPackages.Split(";") | ForEach {
-    choco install $_ -y -force
-}
+#$chocoPackages.Split(";") | ForEach {
+#    choco install $_ -y -force
+#}
 
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\ServerManager" -Name "DoNotOpenServerManagerAtLogon" -Value 1
 
